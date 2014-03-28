79ce038de01634ba807308584a8d994978086b6f
Cassandra Sandquist <robotwholearned88@gmail.com>
2014-03-28 09:26:45 -0600

    start menu

---
+++ RocketCarCanyon/RocketCarCanyon/MenuScene.h
@@ -0,0 +1,13 @@
+//
+//  MenuScene.h
+//  RocketCarCanyon
+//
+//  Created by Cassandra Sandquist on 28/03/2014.
+//  Copyright (c) 2014 Cassandra Sandquist. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MenuScene : SKScene
+
+@end
